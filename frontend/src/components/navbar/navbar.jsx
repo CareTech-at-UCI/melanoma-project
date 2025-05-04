dd57b2e1a86576e104056385471b6644755c805d
Himal <himalm100@gmail.com>
2025-05-03 20:42:25 -0700

    Added Footer

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
-export const navbar = () => {
-  return (
-    <div>navbar</div>
-  )
+
+
+export const Navbar = () => {
+  
+
 }
+
+
+export const Footer = () => {
+  return (
+      <footer className="bg-white text-center p-4">
+          <p className="text-[16px] text-amber-800">
+              Courtesy of the CareTech Team <br />
+              Special thanks to these projects, which inspired Melanoma Detector <br />
+              © 2025 Melanoma Detector by CareTech 
+          </p>
+
+      </footer>
+  );
+}
+
+
+
+
+// export const navbar = () => {
+//   return (
+//     <div>navbar</div>
+//   )
+// }
+
+
