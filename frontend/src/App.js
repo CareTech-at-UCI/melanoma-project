102dda2f38f11832caf70f33c2ef21c8a06aa3cb
Han Diedrich <Hanjdiedrich@gmail.com>
2025-05-05 17:22:51 -0700

    Created base scan page and scan folder, added /scan to router in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Scan } from './pages/Scan';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/scan" element={<Scan />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/Scan.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Scan = () => {
+  return (
+    <h1 className="font-gantari text-body text-main-brown text-3xl font-bold underline">
+      Scanning Page
+    </h1>
+  )
+}
