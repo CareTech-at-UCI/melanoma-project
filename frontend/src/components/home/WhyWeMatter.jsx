7bbb1d42ab617a29f52dfba9fcff002702fb575c
NoNathan17 <nathanong1217@gmail.com>
2025-05-07 23:51:09 -0700

    mobile responsiveness for why we matter page

--- frontend/src/components/home/WhyWeMatter.jsx
+++ frontend/src/components/home/WhyWeMatter.jsx
@@ -4,29 +4,25 @@ export const WhyWeMatter = () => {
   return (
     <section
       id="why-we-matter"
-      className="text-main-brown bg-[#DECEC2] py-16 md:py-24 p-8 md:p-16"
+      className="h-screen md:h-[70vh] flex flex-col md:flex-row text-main-brown bg-[#B59988]"
     >
-      <div className="container mx-auto flex flex-col md:flex-row items-center gap-12">
+      <div className="h-full mx-auto container flex flex-col md:flex-row items-center py-16 md:py-0 p-8 md:p-0">
         {/* Text Content */}
-        <div className="md:w-1/2 text-center md:text-left">
-          <h2 className="text-header-2 mb-6">Why We Matter.</h2>
-          <p className="text-body mb-8">
+        <div className="md:w-full p-0 md:px-20 text-left space-y-5">
+          <h2 className="text-header-2 md:text-header-1">Why <i>We</i> Matter.</h2>
+          <p className="text-body text-base md:text-2xl">
             A paragraph about why the application is important and any
             additional information related to it. This will be as long or as
             short as you need it.
           </p>
-          <button className="text-orange-500 text-xl font-bold bg-white border border-main-orange py-3 px-8 rounded-full hover:bg-orange-500 hover:text-white transition-colors">
+          <button className="text-orange-500 text-lg md:text-xl font-bold bg-white border border-main-orange py-2 md:py-3 px-4 md:px-8 rounded-full hover:bg-orange-500 hover:text-white transition-colors">
             Professional Assistance &gt;
           </button>
         </div>
-        {/* Image */}
-        <div className="md:w-1/2 flex justify-center items-center mt-8 md:mt-0">
-          <img
-            src="/assets/why-we-matter.png"
-            alt="People connecting"
-            className="max-w-full h-auto md:max-w-md lg:max-w-lg"
-          />
-        </div>
+      </div>
+      {/* Image */}
+      <div className="w-full bg-cover bg-center bg-no-repeat h-full"
+        style={{ backgroundImage: "url('/assets/why-we-matter.png')" }}>
       </div>
     </section>
   );
