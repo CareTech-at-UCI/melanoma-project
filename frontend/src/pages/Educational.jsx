c6982d7e4f8621c67886f5f53894073f089ab2cd
Audrey Lu <audreyylu04@gmail.com>
2025-05-08 12:51:45 -0700

    Add bold text

--- frontend/src/pages/Educational.jsx
+++ frontend/src/pages/Educational.jsx
@@ -10,11 +10,36 @@ export const Educational = () => {
             <h2 className="text-header-2">ABCDE of Skin Cancer</h2>
             <p>A straightforward way to identify early melanoma risk and take steps to prevent further skin damage.</p>
             <div className="flex">
-                <AbcdeCard letter="A" imageSrc="/educational/asymmetric.jpg" title="Asymmetry" description="One half of the spot is unlike or different the other half." />
-                <AbcdeCard letter="B" imageSrc="/educational/border.jpg" title="Border" description="The spot has an irregular or ragged border." />
-                <AbcdeCard letter="C" imageSrc="/educational/color.jpg" title="Color" description="The spot contains a variation of distinct colors within it." />
-                <AbcdeCard letter="D" imageSrc="/educational/diameter.png" title="Diameter" description="Above 6 millimeters or ~1/4 inches in size (may vary)." />
-                <AbcdeCard letter="E" imageSrc="/educational/evolution.jpg" title="Evolution" description="The spot differs from other spots or changes in shape, size, texture, or color." />
+                <AbcdeCard
+                  letter="A" 
+                  imageSrc="/educational/asymmetric.jpg" 
+                  title="Asymmetry" 
+                  description={<span>One half of the spot is <b>unlike</b> or <b>different</b> from the other half.</span>} 
+                />
+                <AbcdeCard
+                  letter="B" 
+                  imageSrc="/educational/border.jpg" 
+                  title="Border" 
+                  description={<span>The spot has an <b>irregular</b> or <b>ragged border</b>.</span>}
+                />
+                <AbcdeCard
+                  letter="C"
+                  imageSrc="/educational/color.jpg"
+                  title="Color"
+                  description={<span>The spot contains a <b>variation</b> of <b>distinct colors</b> within it.</span>}
+                />
+                <AbcdeCard
+                  letter="D"
+                  imageSrc="/educational/diameter.png"
+                  title="Diameter"
+                  description={<span>Above <b>6 millimeters</b> or <b>~1/4 inches</b> in <b>size</b> (may vary).</span>}
+                />
+                <AbcdeCard
+                  letter="E"
+                  imageSrc="/educational/evolution.jpg"
+                  title="Evolution"
+                  description={<span>The spot <b>differs from other spots</b> or <b>changes</b> in <b>shape</b>, <b>size</b>, <b>texture</b>, or <b>color</b>.</span>}
+                />
             </div>
         </div>
     </div>
