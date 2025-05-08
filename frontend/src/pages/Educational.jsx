dc9b9a27ca7c040feb5fad248492ceab9524ab63
Audrey Lu <audreyylu04@gmail.com>
2025-05-08 11:48:03 -0700

    Change font to gantari and images flex row

--- frontend/src/pages/Educational.jsx
+++ frontend/src/pages/Educational.jsx
@@ -3,13 +3,13 @@ import { AbcdeCard } from '../components/educational/AbcdeCard';
 
 export const Educational = () => {
   return (
-    <div>
+    <div className="font-gantari text-main-brown">
         <div></div>
-        <div>
+        <div className="bg-[rgba(240,239,237,1)]">
             <img src="/educational/doubleLoopLine.png" alt="double loop line" />
-            <h2>ABCDE of Skin Cancer</h2>
+            <h2 className="text-header-2">ABCDE of Skin Cancer</h2>
             <p>A straightforward way to identify early melanoma risk and take steps to prevent further skin damage.</p>
-            <div>
+            <div className="flex">
                 <AbcdeCard letter="A" imageSrc="/educational/asymmetric.jpg" title="Asymmetry" description="One half of the spot is unlike or different the other half." />
                 <AbcdeCard letter="B" imageSrc="/educational/border.jpg" title="Border" description="The spot has an irregular or ragged border." />
                 <AbcdeCard letter="C" imageSrc="/educational/color.jpg" title="Color" description="The spot contains a variation of distinct colors within it." />
