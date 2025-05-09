a06bd099f74f5896a99afd1633d1d39be66f59bb
Han Diedrich <Hanjdiedrich@gmail.com>
2025-05-09 02:00:38 -0700

    Made responsive, added signatures to custom tailwind fonts styles

--- frontend/src/components/scan/step.jsx
+++ frontend/src/components/scan/step.jsx
@@ -4,29 +4,27 @@ const StepCard = ({title, description, icon: Icon }) => {
     return (
         <div className="
             bg-cream
-            w-[326px] 
-            h-[124px]
-            md:h-[200px] 
+            w-full
+            h-full
             rounded-[8px] 
-            p-[16px] 
+            p-[1.5%] 
             shadow-[0px_8px_8px_0px_rgba(0,0,0,0.15)]
             flex 
             flex-col
             items-center
-            gap-[10px]
         ">
             {/*step content*/}
-            <div className="w-[294px] h-[140px] flex flex-col gap-[8px]">
+            <div className="w-[95%] h-full flex flex-col gap-[8px]">
                 {/*icon*/}
                 <div className="w-[32px] h-[30px] bg-light-brown flex flex-col items-center justify-center" style={{borderRadius: "50%"}}>
                     <Icon/>
                 </div>
                 {/*step-text*/}
-                <div className="w-[294px] h-[55px] md:h-[102px] flex flex-col gap-[8px] md:gap-[16px]">
-                    <h1 className="font-gantari text-main-brown text-header-7 md:text-header-5">
+                <div className="w-[95%] h-[50%] flex flex-col gap-[8px] md:gap-[16px]">
+                    <h1 className="font-gantari text-main-brown text-scan-header-7 md:text-scan-header-5">
                         {title}
                     </h1>
-                    <h1 className="font-gantari text-main-brown text-header-8 md:text-header-6">
+                    <h1 className="font-gantari text-main-brown text-scan-header-8 md:text-scan-header-6">
                         {description}
                     </h1>
                 </div>
--- frontend/src/pages/Scan.jsx
+++ frontend/src/pages/Scan.jsx
@@ -6,22 +6,23 @@ import Upload from '../components/scan/upload'
 
 export const Scan = () => {
   return (
-    <div className="bg-off-white w-full h-full flex flex-col items-center">
+    <div className="bg-off-white w-full h-full flex flex-col items-center overflow-hidden">
         {/*Background gradient*/}
-        <div className="fixed w-full h-full inset-0 bg-cover bg-center opacity-50" style={{backgroundImage: `url(${gradient})`}}>
+        <div className="fixed w-full h-full inset-0 bg-cover bg-center opacity-30" style={{backgroundImage: `url(${gradient})`}}>
         </div>
         {/*Frame 44*/}
-        <div className="absolute top-[115px] w-[326px] h-[694px] md:top-[175px] md:w-[1271px] md:h-[747px] flex flex-col items-center ">
-            {/*Frame 135*/}
-            <div className="text-left w-[334px] h-[41px] md:w-[1266px] md:h-[58px] ">
-                <h1 className="font-gantari text-main-brown text-header-9 md:text-header-1">
-                    Scan your skin.
-                </h1>
-            </div>
+        <div className="absolute top-[5vh] w-[80vw] h-[90vh] md:top-[10vh] md:w-[70vw] md:h-[85vh] flex flex-col items-center justify-center gap-[3%]">
+            
             {/*Frame 43/46*/}
-            <div className="absolute bottom-0 left-0 w-[326px] h-[626px] md:w-[1271px] md:h-[653px] flex flex-col items-center justify-center gap-[16px] md:gap-[36px]">
+            <div className="w-full h-[90%] md:w-[100%] md:h-[90%] flex flex-col items-center gap-[3%]">
+                {/*Frame 135*/}
+                <div className="text-left w-full">
+                    <h1 className="font-gantari text-main-brown text-scan-header-9 md:text-header-1">
+                        Scan your skin.
+                    </h1>
+                </div>
                 {/*steps*/}
-                <div className="w-full flex flex-col md:flex-row items-center justify-center gap-[8px] md:gap-[32px]">
+                <div className="w-full flex flex-col md:flex-row items-center justify-center gap-[8px] md:gap-[3%]">
                     {/*Step 1*/}
                     <StepCard title="Step 1: Take a Photo" description="Use a device to take a photo of the affected area of skin, ensuring that the entire area has been captured." icon={CameraIcon}/>
                     {/*Step 2*/}
@@ -31,7 +32,7 @@ export const Scan = () => {
                 </div>
 
                 {/*upload-image-component*/}
-                <div className="bg-[#F5F5F5] w-[289px] h-[222px] md:w-[994px] md:h-[417px] border-2 border-solid border-[#B59988] rounded-xl min-h-fit">
+                <div className="bg-[#F5F5F5] w-[90%] h-[30%] md:w-[80%] md:h-[60%] border-2 border-solid border-[#B59988] rounded-xl min-h-fit">
                     <p className='font-gantari text-main-brown pl-[5%] pt-[5%]'>Upload Image</p>
                     <Upload/>
                 </div>
